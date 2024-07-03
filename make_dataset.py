615d643d6a9fe2ae4475c70e6b963e12c0dc6e38
Komiiiii-2039 <komichi.1014@gmail.com>
2024-07-03 11:41:38 +0900

    rearrange dataset

--- make_dataset.py
+++ make_dataset.py
@@ -1,21 +1,19 @@
 import os
 import glob
 from PIL import Image
-import numpy as np
 import torch
 from torchvision import transforms
-from sklearn.model_selection import train_test_split
 
 # クラスと設定
-classes = ["normal", "unmarked_crown"]
+classes = ["crown_normal", "crown_police"]
 num_classes = len(classes)
 image_size = 128
 
 # データセットのディレクトリ
 datadir = "./images"
 
-# 画像の前処理
-transform = transforms.Compose([
+# 基本の画像の前処理
+base_transform = transforms.Compose([
     transforms.Resize((image_size, image_size)),
     transforms.ToTensor(),
     transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
@@ -29,33 +27,38 @@ labels = []
 for index, classlabel in enumerate(classes):
     image_dir = os.path.join(datadir, classlabel)
     files = glob.glob(os.path.join(image_dir, "*"))
-    if len(files) == 0 :
+    if len(files) == 0:
         print(f"No files in {image_dir}")
         continue
     for file in files:
         image = Image.open(file)
         image = image.convert("RGB")
-        image = image.resize((image_size, image_size))
 
         # オリジナル画像
-        data.append(transform(image))
+        data.append(base_transform(image))
         labels.append(index)
 
         # 回転
-        for angle in range(-20, 20, 5):
+        for angle in range(-20, 21, 5):
             img_r = image.rotate(angle)
-            data.append(transform(img_r))
+            data.append(base_transform(img_r))
             labels.append(index)
 
         # 反転
         img_trans = image.transpose(Image.FLIP_LEFT_RIGHT)
-        data.append(transform(img_trans))
+        data.append(base_transform(img_trans))
         labels.append(index)
-    print(f"Create dataset for {classlabel}")
+
+print(f"Original and augmented dataset created.")
 
 # テンソルに変換
 data = torch.stack(data)
 labels = torch.tensor(labels)
 
+# 各クラスのデータ数を表示
+for label in range(num_classes):
+    print(f"{classes[label]}: {torch.sum(labels == label)}")
+
 # データセットを保存
 torch.save((data, labels), 'crown.pt')
+print("Dataset augmented and saved successfully.")
